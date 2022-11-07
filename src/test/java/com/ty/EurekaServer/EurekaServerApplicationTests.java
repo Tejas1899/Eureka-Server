f5bc15adf9f20e8d3c48dbef90a6f88fae8f4b1b
Tejas1899 <tejgowda06@gmail.com>
2022-11-08 00:53:46 +0530

    Eurekaserver

---
+++ src/test/java/com/ty/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
